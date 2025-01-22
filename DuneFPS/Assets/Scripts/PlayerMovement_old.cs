9ffc1ed847168a509e9d7dbdf9c6c5574f890bed
Dominic Cupidon <dominicjcupidon@gmail.com>
2025-01-21 18:33:16 -0800

    Cleaned up duplicate scripts

--- DuneFPS/Assets/Scripts/PlayerMovement_old.cs
+++ DuneFPS/Assets/Scripts/PlayerMovement_old.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement_old : MonoBehaviour
 {
     private float speed = 5.0f;
     private float horizontalInput;
